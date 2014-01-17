8819ffea21bb463ed36beae66b2f4c86c093c30e
Jamie Kowalski <jamie.kowalski@gmail.com>
2014-01-17 16:23:06 -0500

    Now inserts straight quotes within code elements

--- smart quotes.ftplugin/README.md
+++ smart quotes.ftplugin/README.md
@@ -3,5 +3,3 @@
 Inserts smart quotes instead of straight quotes in FoldingText documents.
 
 Requires FoldingText 1.3
-
-Know issue: smart quotes are inserted inside of code blocks.
--- smart quotes.ftplugin/main.js
+++ smart quotes.ftplugin/main.js
@@ -2,38 +2,58 @@ define(function(require, exports, module) {
 	var Extensions = require('ft/core/extensions');
 
     function insertSmartQuote(editor, type_arg) {
-        var selectedRange = editor.getSelectedRange();
-        var length = selectedRange.length();
-        var type = type_arg.charAt(0);
-        var direction = 'l';
+        var selectedRange = editor.getSelectedRange(),
+            length = selectedRange.length(),
+            quoteType = type_arg.charAt(0),
+            direction = 'l',
+            lsquo = '‘',
+            rsquo = '’',
+            ldquo = '“',
+            rdquo = '”',
+            quote = '';
         
-        // if (! selectedRange.collapsed() ) return;
+        var node = selectedRange.startNode,
+            attributes = node.lineAttributesAtIndex(selectedRange.startOffset),
+            nodeType = node.type();
         
-        beforeText = selectedRange.rangeByOffsetting(-1, 1 - length).textInRange();
-        // afterText = selectedRange.rangeByOffsetting(1, 1).textInRange();
-        if (beforeText.match(/\S/)) {   // OR /[\\.,>;:?!“”‘’—\*`%\])}]/
-            direction = 'r';
-        }
-        
-        sq = '';          // sq for smart quote
-        
-        if (type === 's') {
-            if (direction === 'r') {
-                sq = "’"
-            } else {
-                sq = "‘"
-            }
-        } else if (type === 'd') {
-            if (direction === 'r') {
-                sq = '”'
-            } else {
-                sq = '“'
-            }
+        if ( nodeType === 'codeblock' ||
+             nodeType === 'fencedcode' ||
+             nodeType === 'htmlblock' ||
+             (attributes && attributes.code !== undefined) ) {
+                 if (quoteType === 's') {
+                     quote = "'"
+                 } else if (quoteType === 's') {
+                     quote = '"'
+                 } else {
+                     return;
+                 }
         } else {
-            return;
+            beforeText = selectedRange.rangeByOffsetting(-1, 
+                1 - length).textInRange();
+            // afterText = selectedRange.rangeByOffsetting(1, 1).textInRange();
+            if (beforeText.match(/\S/)) {   // OR /[\\.,>;:?!“”‘’—\*`%\])}]/
+                direction = 'r';
+            }
+                
+            if (quoteType === 's') {
+                if (direction === 'r') {
+                    quote = rsquo
+                } else {
+                    quote = lsquo
+                }
+            } else if (quoteType === 'd') {
+                if (direction === 'r') {
+                    quote = rdquo
+                } else {
+                    quote = ldquo
+                }
+            } else {
+                return;
+            }
         }
-        editor.replaceSelection(sq);
-        editor.setSelectedRange(editor.getSelectedRange().rangeByOffsetting(+1, -sq.length));
+        
+        editor.replaceSelection(quote, 'end');
+        // editor.setSelectedRange(editor.getSelectedRange().rangeByOffsetting(+1, -quote.length));
 	}
     
     Extensions.add('com.foldingtext.editor.init', function(editor) {
