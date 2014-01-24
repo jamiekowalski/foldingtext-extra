7c682a17545cfd0a343592ce9b940e784fca0ea2
jamiekowalski <jamie.kowalski@gmail.com>
2014-01-23 22:06:35 -0500

    Add support for non-English quotation styles

--- smart quotes.ftplugin/README.md
+++ smart quotes.ftplugin/README.md
@@ -2,9 +2,23 @@
 
 Inserts smart quotes instead of straight quotes in FoldingText documents.
 
-In most cases, the plugin will insert the quote you want; but note that it only looks at the previous character to determine whether to insert a left- or right-quote, and makes no attempt to match pairs.
+Includes support for the following quotation styles:
 
-It fails in the following cases:
+“English”  
+„German“  
+»German Guillemets«  
+«French»  
+« French with spaces » (automatically inserts a non-breaking space)
+
+Also includes some support for non-US keyboard layouts: German and Russian layouts use Shift-2 and Shift-3 for quotes. The way CodeMirror allows defining keyboard shortcuts makes it difficult to assign shortcuts for non-US keyboards.
+
+Set the default language and keyboard in the script (look for the text `sq.defaultLanguage = 'en'` near the top). This plugin adds two commands: `smart quotes change language` that allows you to choose a new style of quotation marks without editing the script. Use the following codes (corresponding to the styles listed above): `en` `de` `degu` `fr` `frsp`. `smart quotes toggle` allows you to turn off smart quotes.
+
+In most cases, the plugin will insert the quote you want; but it only looks at the previous character to determine whether to insert a left- or right-quote, and makes no attempt to match pairs.
+
+Note that the plugin cannot distinguish between single closing quotation marks and apostrophes in languages (e.g. German) in which the two are different. In that case, apostrophes must be entered manually.
+
+It also fails in the following cases:
 
 “Still”—he thought—”it might turn out all right.” (Quotation mark after dash; can't simply always use left quote, since a more common paring of dash and quote is: “That’s all well and good—”)   
 In ‘99   
--- smart quotes.ftplugin/main.js
+++ smart quotes.ftplugin/main.js
@@ -1,14 +1,73 @@
 define(function(require, exports, module) {
+
+    if (! window.co) window.co = {}; if (! co.jamiekowalski) co.jamiekowalski = {}
+    if (! co.jamiekowalski.SmartQuotes) co.jamiekowalski.SmartQuotes = {}
+    var sq = co.jamiekowalski.SmartQuotes;
+    
+    sq.defaultLanguage = 'en';  // 'en', 'de', 'degu', 'fr', 'frsp'
+    sq.defaultKeyboard = 'en';  // 'en', 'de', 'ru'
+    sq.enabled = true;
+    
+    sq.glyphSets = {
+        en: { ls: '‘', rs: '’', ld: '“', rd: '”', desc: 'English'},
+        de: { ls: '‚', rs: '‘', ld: '„', rd: '“', desc: 'German'},
+        degu: { ls: '›', rs: '‹', ld: '»', rd: '«', desc: 'German guillemets' },
+        fr: { ls: '‹', rs: '›', ld: '«', rd: '»', desc: 'French' },
+        frsp: { ls: '‹ ', rs: ' ›', ld: '« ', rd: ' »', desc: 'French with spaces' }
+    }
+    sq.shortcutSets = {
+        en: { s: "'", d: "Shift-'" },
+        de: { s: "Shift-3", d: "Shift-'" },
+        ru: { s: "Shift-3", d: "Shift-'" }
+        // Codemirror keymaps are difficult with non-US keyboards
+    }
+
 	var Extensions = require('ft/core/extensions');
 
-    function insertSmartQuote(editor, type_arg) {
+    function defineSmartQuotes(editor, lang, kb) {
+        var sq = co.jamiekowalski.SmartQuotes,
+            keyMap = {},
+            language,
+            keyboard,
+            glyphSet,
+            shortcuts
+        
+        if (lang && sq.glyphSets[lang]) {
+            language = lang
+        } else {
+            language = sq.defaultLanguage
+        }
+        
+        if (kb && sq.shortcutSets[kb]) {
+            keyboard = kb
+        } else {
+            keyboard = sq.defaultKeyboard
+        }
+        
+        glyphSet = sq.glyphSets[language]
+        shortcuts = sq.shortcutSets[keyboard]
+        
+        keyMap.name = keyboard
+        keyMap[shortcuts.s] = function(editor) {
+                insertSmartQuote(editor, 's', glyphSet);
+        };
+        keyMap[shortcuts.d] = function(editor) {
+                insertSmartQuote(editor, 'd', glyphSet);
+        }
+        
+        if (sq.keyMap) {
+            editor.removeKeyMap(sq.keyMap.name);
+        }
+        sq.keyMap = keyMap;
+        if (sq.enabled) {
+            editor.addKeyMap(keyMap);
+        }
+    }
+
+    function insertSmartQuote(editor, type_arg, qGlyphs) {
         var selectedRange = editor.getSelectedRange(),
             length = selectedRange.length(),
             quoteType = type_arg.charAt(0),
-            lsquo = '‘',
-            rsquo = '’',
-            ldquo = '“',
-            rdquo = '”',
             quote = '';
         
         // check whether we're in a code element
@@ -31,26 +90,31 @@ define(function(require, exports, module) {
                  }
         } else {
             // we're in normal text
+            var direction;
             
-            beforeChar = selectedRange.rangeByOffsetting(-1, 
-                1 - length).textInRange();
-            
-            var direction = 'r';
-            if (beforeChar.match(/^[\s\/[({]?$/)) {
+            if (selectedRange.startOffset === 0) {
+                // at beginning of document
                 direction = 'l';
+            } else {
+                beforeChar = selectedRange.rangeByOffsetting(-1, 
+                    1 - length).textInRange();
+                direction = 'r';
+                if (beforeChar.match(/^[\s\/[({]?$/)) {
+                    direction = 'l';
+                }
             }
-                
+            
             if (quoteType === 's') {
                 if (direction === 'r') {
-                    quote = rsquo
+                    quote = qGlyphs.rs
                 } else {
-                    quote = lsquo
+                    quote = qGlyphs.ls
                 }
             } else if (quoteType === 'd') {
                 if (direction === 'r') {
-                    quote = rdquo
+                    quote = qGlyphs.rd
                 } else {
-                    quote = ldquo
+                    quote = qGlyphs.ld
                 }
             } else {
                 return;
@@ -60,14 +124,41 @@ define(function(require, exports, module) {
         editor.replaceSelection(quote, 'end');
 	}
     
-    Extensions.add('com.foldingtext.editor.init', function(editor) {
-        editor.addKeyMap({
-            "Shift-'" : function(editor) {
-                insertSmartQuote(editor, 'double');
-            },
-            "'" : function(editor) {
-                insertSmartQuote(editor, 'single');
-            }
-        });
+    Extensions.add('com.foldingtext.editor.init', function(editor) {        
+        defineSmartQuotes(editor) // no additional args: use defaults
     });
+    
+	Extensions.add('com.foldingtext.editor.commands', {
+		name: 'smart quotes language',
+		description: 'Change language for smart quotes',
+		performCommand: function (editor) {
+            var gs = co.jamiekowalski.SmartQuotes.glyphSets
+            var promptStr = 'Set language using one of the following codes:\n\n'
+            for (var lang in gs) {
+                promptStr += lang + ' - ' + gs[lang].ld + gs[lang].desc + 
+                    gs[lang].rd + '\n'
+            }
+            promptStr = promptStr.trim()
+            
+            var lang = prompt(promptStr)
+            if (lang) {
+                defineSmartQuotes(editor, lang, 'en');
+            }
+		}
+	});
+    
+	Extensions.add('com.foldingtext.editor.commands', {
+		name: 'smart quotes toggle',
+		description: 'Toggle smart quotes on and off',
+		performCommand: function (editor) {
+            var sq = co.jamiekowalski.SmartQuotes;
+            if (sq.enabled) {
+                editor.removeKeyMap(sq.keyMap.name);
+                sq.enabled = false;
+            } else {
+                editor.addKeyMap(sq.keyMap);
+                sq.enabled = true;
+            }
+		}
+	});
 });
