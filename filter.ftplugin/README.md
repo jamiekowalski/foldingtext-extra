4006ac353449ba620dfef569830e5957ac483633
Jamie Kowalski <jamie.kowalski@gmail.com>
2014-02-01 11:18:36 -0500

    Added filter plugin

---
+++ filter.ftplugin/README.md
@@ -0,0 +1,70 @@
+# Filter: FoldingText Extension
+
+Filters a FoldingText document based on an expression given in a simple syntax. For example, the expression `;writ/fiction/@todo` will show only items tagged @todo that are under an item containing the text 'fiction' that is under an heading with the text 'writ' (including partial word matches).
+
+The extension can also be triggered with the keyboard shortcut of Shift+Command+' (this can be changed near the end of the main.js file).
+
+If the path cannot be parsed, a message is logged to the console. (Access the console in FoldingText by holding the Option key when you launch the app; then right-click within the editor and choose 'Inspect Element'.)
+
+Requires FoldingText 1.3 build 716 or later.
+
+## Examples of expressions:
+
+`@important`
+: items tagged @important. Note that tag matches must be exact; `@im` will not match an item tagged `@important`. This is in contrast to text matches.
+
+`not @done`
+: items not tagged @done. Note that currently, if an item is tagged @done but has subitems, it will not be hidden.
+
+`;misc`
+: headings that contain the text 'misc'
+
+`;misc items`
+: headings that contain both 'misc' and 'items'
+
+`;misc or items`
+: headings that contain either 'misc' or 'items'
+
+`misc/today tomorrow`
+: items that contain both 'today' and 'tomorrow under an item that contains 'misc'.
+
+`;code;fold text`
+: heading that contains both 'fold' and 'text' under a heading that contains 'code'.
+
+`yesterday today and tomorrow`
+: items that contains 'yesterday' and 'today' and 'tomorrow'. When only one boolean operator is used with three or more terms, this operator is used as the default.
+
+`yesterday or today and tomorrow`
+: items that contains 'yesterday' and 'today' and 'tomorrow'. When a mix of 'and' and 'or' is used, 'or's are changed to 'and'.
+
+`#cm`
+: items that contain a Critic Markup command/annotation
+
+`#hl`
+: items that contain a Critic Markup highlight
+
+`#dl` or `#del`
+: items that contain a Critic Markup deletion
+
+`#in` or `#ins`
+: items that contain a Critic Markup insertion
+
+`#em` or `#emph`
+: items that contain a Markdown `em` element
+
+`#st` or `#str` or `#strong`
+: items that contain a Markdown `strong` element
+
+any expression starting with `>`
+: don't show ancestors
+
+any expression starting with `/`
+: use FoldingText's full XPath syntax instead of the custom syntax
+
+## Known Issues
+
+Terms containing characters other than [a-zA-Z] must be wrapped in quotes.
+
+Cannot undo changes to text in the panel.
+
+The escape key closes the panel on keyup, not keydown, resulting in an apparent lag.
---
+++ filter.ftplugin/main.js
@@ -0,0 +1,193 @@
+define(function(require, exports, module) {
+    'use strict';
+    require('./panel.js');
+	var Extensions = require('ft/core/extensions'),
+        NodePath = require('ft/core/nodepath').NodePath,
+        Editor,
+        debug = false
+    
+    function insert_ops( string, custom_default ) {
+        string = string.trim()
+        if (! string.match(/ /)) return string  // if no spaces, return immediately
+
+        var protector = '=_=',
+            ops = [
+                'and',
+                'or'
+            ],
+            default_op
+
+        if (string.match(ops[0])) {
+            default_op = ops[0]
+        } else if (string.match(ops[1])) {
+            default_op = ops[1]
+        } else if (custom_default) {
+            default_op = custom_default
+        } else {
+            default_op = ops[1]
+        }
+
+        // protect 'not'
+        string = string.replace(/\b(not)\b\s*/g, '$1' + protector)
+    
+        var ops_find = new RegExp('\\s*\\b(' + ops.join('|') + ')\\b\\s*', 'g')
+        var ops_replace = ' '
+        var space_delim_find = /\s+/g
+
+        // remove existing ops
+        string = string.replace(ops_find, ops_replace)
+        string = string.trim()
+
+        // insert default
+        string = string.replace(space_delim_find, ' ' + default_op + ' ')
+    
+        // un-protect 'not'
+        string = string.replace(new RegExp(protector, 'g'), ' ')
+
+        return string.trim()
+    }
+
+    function parse_segment( input, heading_marker ) {
+        var s = input.trim(),
+            defaultOp = 'and',
+            heading_marker_regex = new RegExp( '^' + heading_marker + '\\s*' )
+    
+        if ( ! s ) {
+            return ''
+        }
+    
+        s = s.replace(/#?\b([^\s~<>=]+)\s*([~<>=]+)\s*/g, '#$1$2')
+        // s = s.replace(/\(#[^\s~<>=]+[~<>=]+[^(]+/g, '$&)')
+    
+        s = insert_ops( s, defaultOp )
+
+        if ( s.match( heading_marker_regex ) ) { // heading
+            s = s.replace( heading_marker_regex, '' )
+            s = '(' + s + ')'
+            s += ' and @type=heading'
+        }
+
+        // TODO pull out property queries first
+
+        s = s.replace(/#cm\b/g, '@line:comment')
+        s = s.replace(/#hl\b/g, '@line:highlight')
+        s = s.replace(/#de?l\b/g, '@line:del')
+        s = s.replace(/#ins?\b/g, '@line:ins')
+        s = s.replace(/#str?o?n?g?\b/g, '@line:strong')
+        s = s.replace(/#em(ph)?\b/g, '@line:em')
+        s = s.replace(/#([^\s:]+)\b/g, '@property:$1')
+        s = s.replace(/(@property:[^\s:]+)~(\S+)\b/g, '$1 contains $2')
+    
+        return s
+    }
+
+    function parse_path( input ) {
+        var input = input.trim(),
+            delim = '/',
+            heading_marker = ';',
+            ancestors_off = '>',
+            ancestors,
+            descendants
+    
+        if (! input) {
+            return input
+        } else if (input.charAt(0) === '/') {
+            // assume full XPath; leave it alone
+            return input
+        }
+        
+        // set ancestors option
+        ancestors = true;
+        if (input.charAt(0) === ancestors_off) {
+            ancestors = false
+            input = input.slice(1)
+        }
+
+        // set descendants option
+        descendants = false;
+        if (input.match(new RegExp('[' + delim + heading_marker + ']\\s*$'))) {    // [:\/]\s*$
+            descendants = true;
+            input = input.slice(0, -1);
+        }
+
+        var heading_delim_find = new RegExp('([^' + delim + '\\s])\\s*(' +
+            heading_marker + ')', 'g')
+
+        // place delimiter before heading marker if not there
+        input = input.replace(heading_delim_find, '$1' + delim + '$2');
+
+        var segments = input.split(new RegExp('\\s*' + delim + '\\s*'))
+        var path = ''
+ 
+        segments.forEach(function(segment) {
+            path += '//' + parse_segment(segment, heading_marker);
+        });
+        
+        if (path.match(/heading$/)) { // last segment is a heading // FIXME magic
+            descendants = true
+        }
+    
+        if (descendants) {
+            path += '///*'
+        }
+        if (ancestors) { // due to bug in 715, can't have both 
+                                // ancestors and descendants
+            path += '/ancestor-or-self::*'
+        }
+    
+        return path
+    }
+    
+    function filter_by_path(path) {
+        if (path && ! path.match(/^ +$/)) {
+            
+            var parse_result = NodePath.parse(path)
+            if (parse_result.errorColumn > -1) {
+                console.log(path + '\n' + "Path failed at: " +
+                    parse_result.errorColumn)
+            } else {
+                if (debug) console.log(path)
+                Editor.setNodePath(path)
+            }
+        } else {
+            Editor.performCommand('focusOut') // TODO should only focus out if not already focused out
+        }
+    }
+    
+    Extensions.add('com.foldingtext.editor.init', function(editor) {
+        Editor = editor
+        
+        JKPanel.addPanel({
+            className: 'JKFilterPanel',
+            placeholder: 'enter expression...',
+            onreturn: function(panelEle, input) {
+                filter_by_path(parse_path(input.value))
+            },
+            onescape: function(panelEle, input) {
+                // input.dispatchEvent(new CustomEvent('blur'))
+                JKPanel.hidePanel(false)
+                // editor.performCommand('focusOut')
+            },
+            onchange: function(panelEle, input) {
+                filter_by_path(parse_path(input.value))
+            }
+        });
+        
+        editor.addKeyMap({
+                        
+            "Shift-Cmd-'" : function (editor) {
+                JKPanel.togglePanel()
+            }
+            
+            /* Info about keyboard shortcuts
+             * from http://codemirror.net/doc/manual.html#keymaps
+             * 
+             * Examples of names defined here are Enter, F5, and Q. These can be 
+             * prefixed with Shift-, Cmd-, Ctrl-, and Alt- (in that order!) to 
+             * specify a modifier. So for example, Shift-Ctrl-Space would be a valid
+             * key identifier.
+             */
+            
+        })
+    });
+});
