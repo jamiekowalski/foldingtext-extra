1f4a1aebda9cdb7e4f7c428a6e3f2b4dfab18fca
Jamie Kowalski <jamie.kowalski@gmail.com>
2014-02-01 13:43:35 -0500

    Cleaned up 'toggle critic' plugin for FT 716

--- toggle critic.ftplugin/main.js
+++ toggle critic.ftplugin/main.js
@@ -2,14 +2,7 @@ define(function(require, exports, module) {
 	var Extensions = require('ft/core/extensions'),
         Dom = require('ft/util/dom'),
         // UserDefaults = require('ft/core/userdefaults'),  // FT 716+
-        hideClass = 'hide-critic',
-        shown = true;
-    
-    /*
-    
-    This plugin requires special CSS in user.less
-    
-    */
+        hideClass = 'hide-critic'
 
 	Extensions.add('com.foldingtext.editor.commands', {
 		name: 'toggle critic',
@@ -17,16 +10,8 @@ define(function(require, exports, module) {
 		performCommand: function (editor) {
             // userDefaults.setStringForKey('testing', 'mykey');
             // userDefaults.stringForKey()
-            
-            if (shown) {
-                Dom.addClass(document.body, hideClass);
-                shown = false;
-                editor.refresh();
-            } else {
-                Dom.removeClass(document.body, hideClass);
-                shown = true;
-                editor.refresh();
-            }
+            Dom.toggleClass(document.body, hideClass);
+            editor.refresh();
 		}
 	});
 });
