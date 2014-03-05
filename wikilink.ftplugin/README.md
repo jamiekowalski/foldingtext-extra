b7daa4e27370f9264469c3a41eca104aa8f15993
Jamie Kowalski <jamie.kowalski@gmail.com>
2014-03-04 21:59:41 -0500

    Added fallback folders and other improvements

--- wikilink.ftplugin/README.md
+++ wikilink.ftplugin/README.md
@@ -2,7 +2,11 @@
 
 Enables WikiLink syntax: `[[Link Text]]`.
 
-When the link is clicked, the folder containing the current document will be searched, and the first matched document (with extension `.ft`, `.md`, or `.txt`) will be opened. Currently, this is a strict case-insensitive substring match. If no file is found, the text will be searched within [Notational Velocity](http://notational.net) or [nvALT](http://brettterpstra.com/projects/nvalt/).
+When the link is clicked, the folder containing the current document will be searched, and the first matched document will be opened. The search uses file glob, with each non-word character of the search term replaced with `*`. 
+
+Fallback folders can be specified in the script; if no match was found in the original folder, these folders will be searched successively until a match is found.
+
+If no match is found in any folder, the text will be searched within [Notational Velocity](http://notational.net) or [nvALT](http://brettterpstra.com/projects/nvalt/).
 
 If the link text is prefixed with `#`, clicking the link will focus the corresponding heading in the document; this allows for cross-referencing within the same document.
 
@@ -10,6 +14,8 @@ To explicitly search Notational Velocity, prefix the link text with `@`.
 
 ## To Install
 
-Download the [ZIP of this repository](https://github.com/jamiekowalski/foldingtext-extra/archive/master.zip) and move the `wikilink.ftplugin` to your FoldingText plug-ins folder (open this from FoldingText by choosing File > Open Application Folder). Double-click `Handle FoldingText WikiLinks.app.zip` to unzip it, and delete the .zip file.
+Download the [ZIP of this repository](https://github.com/jamiekowalski/foldingtext-extra/archive/master.zip) and move the `wikilink.ftplugin` to your FoldingText plug-ins folder (open this from FoldingText by choosing File > Open Application Folder).
+
+This extension will most likely fail to run at first because of the Gate Keeper security system. You can circumvent this by right-clicking on the .app, selecting “Open”; when prompted, allow the app to open. The extension should now work.
 
 Requires [FoldingText 2.0](http://support.foldingtext.com/discussions/development-versions/)
--- wikilink.ftplugin/wikilink_search.rb
+++ wikilink.ftplugin/wikilink_search.rb
@@ -1,28 +1,17 @@
 #!/usr/bin/ruby -w
 
-# Used with FoldingText WikiLink Handler (Applescript App)
-
 require 'uri'
+require 'logger'
 
-doc_path = ARGV[0]
-search_term_uri = ARGV[1]
-search_term = URI.decode_www_form_component(search_term_uri)
+search_folders = [
+    '~/Dropbox/Notes/',
+    '~/Dropbox/Notes Archive/',
+    
+]
+extensions = 'md,ft,txt,png,jpg,jpeg,pdf'
 
-doc_folder = doc_path.slice(/^.+\//);
-
-files = Dir[doc_folder + '*.{md,ft,txt,png,jpg,jpeg,pdf}']
-
-target_file = nil
-
-files.each do |file|
-    if (file.match(Regexp.new(Regexp.escape(search_term), Regexp::IGNORECASE)))
-        target_file = file
-        break
-    end
-end
-
-if not target_file.nil?
-    target_file.gsub!(/"/){ %q[\"] }
+def openFile file
+    target_file = file.gsub(/"/){ %q[\"] }
     
     # open the file in the default app for its type
     %x[open "#{target_file}"]
@@ -30,7 +19,47 @@ if not target_file.nil?
     # this next version will always open in FoldingText, but it sometimes fails due
     # to permissions. May be a sandboxing issue
     # %x[osascript -e 'tell application "FoldingText" to open "#{target_file}"']
-else
-    # if file was not found, search for it in Notational Velocity (or nvALT)
-    %x[open "nv://find/#{search_term_uri}"]
+end
+
+def openInNV search_term
+    %x[open "nv://find/#{search_term}"]
+end
+
+if ARGV[0] == '-f'
+
+    search_term_uri = ARGV[1]
+    search_term = URI.decode_www_form_component(search_term_uri)
+    doc_path = ARGV[2]
+    
+    file_glob = search_term.gsub(/[^\w\-]+/, '*') + "*.{#{extensions}}"
+    file_glob.gsub!(/\*+/, '*')
+        
+    if (not doc_path.nil?) and doc_path.length > 0
+        doc_folder = doc_path.slice(/^.+\//)
+        search_folders.unshift doc_folder
+    end
+    
+    target_file = nil
+    search_folders.each do |folder|
+        folder.strip!
+        folder.gsub!(/^~/, Dir.home)
+        folder << '/' if not folder.match(/\/$/)
+        
+        files = Dir.glob(folder + file_glob, File::FNM_CASEFOLD)
+        
+        if files.length > 0
+            target_file = files[0]
+            break
+        end
+    end
+        
+    if not target_file.nil?
+        openFile target_file
+    else
+        # if file was not found, search for it in Notational Velocity (or nvALT)
+        openInNV search_term_uri
+    end
+
+elsif ARGV[0] == '-n'
+    openInNV ARGV[1]
 end
